9245e9c16bf7ea3e48890b73b41d2879dec516de
ceraruz <>
2023-09-13 20:39:27 +0300

    login, logout, refresh token endpoints

--- auth-service/app/src/api/v1/auth.py
+++ auth-service/app/src/api/v1/auth.py
@@ -1,9 +1,10 @@
 from http import HTTPStatus
-
 from fastapi import APIRouter, Depends, HTTPException
+from starlette.responses import Response
 
+from src.api.v1.schemas.auth import LoginResponse
 from src.core.config import logger
-from src.models.data import UserSingUp
+from src.models.data import UserSingUp, UserLogin
 from src.services.auth import AuthService, get_auth_service
 
 router = APIRouter()
@@ -17,26 +18,116 @@ router = APIRouter()
     summary="Регистрация нового пользователя",
 )
 async def register(
-    user_create: UserSingUp, user_service: AuthService = Depends(get_auth_service)
+    user_create: UserSingUp, auth_service: AuthService = Depends(get_auth_service)
 ) -> HTTPStatus:
     logger.info(f"/signup - login: {user_create.login}")
-    user_found = await user_service.get_by_login(user_create.login)
+    user_found = await auth_service.get_by_login(user_create.login)
     if user_found:
         logger.info(f"/signup - login: {user_create.login}, found")
         raise HTTPException(
             status_code=HTTPStatus.BAD_REQUEST, detail='Username is taken'
         )
 
-    user_found = await user_service.get_by_mail(user_create.email)
-    logger.info(f"/signup - emain: {user_create.email}")
+    user_found = await auth_service.get_by_mail(user_create.email)
+    logger.info(f"/signup - email: {user_create.email}")
     if user_found:
         logger.info(f"/signup - email: {user_create.email}, found")
         raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail='Mail is taken')
 
     try:
-        await user_service.add_user(user=user_create)
+        await auth_service.add_user(user=user_create)
         logger.info(f"Signup successful for login: {user_create.login}")
         return HTTPStatus.CREATED
     except Exception as ex:
         logger.error(f"Signup failed due to error: {ex}")
         raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail=str(ex))
+
+
+@router.post(
+    '/login',
+    status_code=HTTPStatus.OK,
+    tags=['auth'],
+    description='Login user',
+    summary="Авторизация пользователя",
+    response_model=LoginResponse,
+)
+async def login(
+    user: UserLogin,
+    auth_service: AuthService = Depends(get_auth_service),
+) -> LoginResponse | Response:
+    logger.info(f"/login user - login: {user.login}")
+    user_found = await auth_service.get_by_login(user.login)
+    if user_found:
+        logger.info(f"/login user - login: {user.login}, found")
+    else:
+        logger.error("user not found")
+        return Response(
+            status_code=HTTPStatus.UNAUTHORIZED, content="Invalid login or password"
+        )
+    try:
+        await auth_service.check_password(user=user)
+        access_token = await auth_service.create_access_token(user_found.email)
+        refresh_token = await auth_service.create_refresh_token(user_found.email)
+        return LoginResponse(access_token=access_token, refresh_token=refresh_token)
+    except ValueError as ex:
+        logger.error(f"login failed due to error: {ex}")
+        return Response(
+            status_code=HTTPStatus.UNAUTHORIZED, content="invalid login or password"
+        )
+
+
+@router.post(
+    '/logout',
+    status_code=HTTPStatus.OK,
+    tags=['auth'],
+    description='Logout user',
+    summary="Выход пользователя из сичтемы",
+)
+async def logout(
+    auth_service: AuthService = Depends(get_auth_service),
+) -> Response:
+    logger.info("/logout user - get access and refresh tokens")
+    await auth_service.auth.jwt_required()
+    await auth_service.auth.jwt_refresh_token_required()
+    subject = await auth_service.auth.get_jwt_subject()
+    logger.info(f"/ get user - login: {subject.login}")
+    user = auth_service.get_by_mail(subject)
+    if user:
+        logger.info(f"/Set expire tokens to redis")
+        await auth_service.revoke_both_tokens(user.login)
+    return Response(HTTPStatus.OK)
+
+
+@router.post(
+    '/refresh_token',
+    status_code=HTTPStatus.OK,
+    tags=['auth'],
+    description='Refresh user tokens',
+    summary="Обновление токенов пользователя",
+    response_model=LoginResponse,
+)
+async def refresh_token(
+    auth_service: AuthService = Depends(get_auth_service),
+) -> Response | LoginResponse:
+    logger.info("/refresh user - get access and refresh tokens")
+    await auth_service.auth.jwt_required()
+    await auth_service.auth.jwt_refresh_token_required()
+    refresh_jti = (await auth_service.auth.auth.get_raw_jwt())['jti']
+
+    jwt_subject = await auth_service.auth.get_jwt_subject()
+    logger.info(f"/ get user - login: {jwt_subject.login}")
+    user = auth_service.get_by_mail(jwt_subject)
+    if not user:
+        return Response(HTTPStatus.UNAUTHORIZED)
+
+    logger.info(f"/Set expire tokens to redis")
+    await auth_service.revoke_both_tokens(user.login)
+    token_is_expired = auth_service.check_token_is_expired(user.login, refresh_jti)
+    if token_is_expired:
+        return Response(HTTPStatus.UNAUTHORIZED)
+    new_access_token = auth_service.create_access_token(subject=jwt_subject)
+    new_refresh_token = auth_service.create_access_token(subject=jwt_subject)
+
+    return LoginResponse(
+        access_token=new_access_token, refresh_token=new_refresh_token
+    )
--- auth-service/app/src/services/auth.py
+++ auth-service/app/src/services/auth.py
@@ -5,19 +5,22 @@ from fastapi import Depends
 from redis.asyncio import Redis
 from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from src.core.config import logger
 from src.db.postgres import get_session
 from src.db.redis import get_redis
-from src.models.data import UserSingUp
+from src.models.data import UserSingUp, UserLogin
 from src.models.user import User
 from src.services.const import CACHE_EXPIRE_REFRESH_TOKEN
 
 
 class AuthService:
-    def __init__(self, redis: Redis, pg: AsyncSession):
+    def __init__(
+        self, redis: Redis, pg: AsyncSession, auth: AuthJWT
+    ):
         self.redis = redis
         self.pg = pg
+        self.auth = auth
 
     async def get_by_login(self, login: str) -> User | None:
         result = await self.pg.execute(select(User).where(User.login == login))
@@ -29,10 +32,12 @@ class AuthService:
         user_found = result.scalars().first()
         return user_found if user_found else None
 
-    async def check_password(self, login: str, password: str) -> bool:
+    async def check_password(self, user: UserLogin) -> bool:
         result = await self.pg.execute(
-            select(User).where((User.password == password) & (User.login == login))
+            select(User.password).where(User.login == user.login)
         )
+        password_hash = result.scalars().first()
+        result = check_password_hash(password_hash, user.password)
         return bool(result)
 
     async def add_user(self, user: UserSingUp) -> None:
@@ -54,16 +59,29 @@ class AuthService:
     async def add_jwt_to_redis(self, login: str, jwt_val: str):
         self.redis.set(f'{login}::{jwt_val}', jwt_val, CACHE_EXPIRE_REFRESH_TOKEN)
 
-    async def revoke_both_tokens(self, login, authorize: AuthJWT) -> None:
-        refresh_jti = (await authorize.get_raw_jwt())['jti']
-        access_jti = (await authorize.get_raw_jwt())['access_jti']
+    async def revoke_both_tokens(self, login) -> None:
+        refresh_jti = (await self.auth.get_raw_jwt())['jti']
+        access_jti = (await self.auth.get_raw_jwt())['access_jti']
         await self.add_jwt_to_redis(login, access_jti)
         await self.add_jwt_to_redis(login, refresh_jti)
+
+    async def create_access_token(self, payload: str):
+        access_token = await self.auth.create_access_token(payload)
+        return access_token
+
+    async def create_refresh_token(self, payload: str):
+        refresh_token = await self.auth.create_refresh_token(payload)
+        return refresh_token
+
+    async def check_token_is_expired(self, login: str, jwt_val: str) -> bool:
+        result = self.redis.get(f'{login}::{jwt_val}')
+        return bool(result)
 
 
 @lru_cache()
 def get_auth_service(
     redis: Redis = Depends(get_redis),
     pg: AsyncSession = Depends(get_session),
+    auth: AuthJWT = Depends()
 ) -> AuthService:
-    return AuthService(redis, pg)
+    return AuthService(redis, pg, auth)
