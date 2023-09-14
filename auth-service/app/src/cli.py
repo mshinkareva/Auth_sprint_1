7a8b2559e79906f255a075c8091af85b9cea29f2
Lappa <lappa@WIN-E9U6DRLADS8>
2023-09-14 13:52:51 +0300

    add sli.py

---
+++ auth-service/app/src/cli.py
@@ -0,0 +1,33 @@
+import os
+
+from typing import Optional
+from dotenv import load_dotenv
+
+import typer
+from typing_extensions import Annotated
+
+from src.db.postgres import get_session_sync
+from src.models.user import User
+
+
+app = typer.Typer()
+
+
+@app.command()
+def create_user(login: str,
+           password: str,
+           email: str,
+           first_name: Annotated[Optional[str], typer.Argument()]= 'Admin',
+           last_name: Annotated[Optional[str], typer.Argument()] = 'Super',
+           ):
+    """
+    Create a new user with USERNAME.
+    """
+    user = User(login=login, password=password, email=email, first_name=first_name, last_name=last_name)
+    with get_session_sync() as session:
+        session.add(user)
+        session.commit()
+
+
+if __name__ == "__main__":
+    app()
